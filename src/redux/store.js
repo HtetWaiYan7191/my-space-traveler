7438c7de826ebe87c625d22ad0d1845163131b9a
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-18 12:15:07 +0630

    Update store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rocketReducer from './rocketSlice';
+import missionReducer from './missionSlice';
 
 const store = configureStore({
   reducer: {
     rockets: rocketReducer,
+    missions: missionReducer,
   },
 });
 
