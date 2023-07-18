ee2b160e15e1500ab787516ccc6235c22f2d1dab
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-18 11:14:11 +0630

    fetch rockdata in rocketSlice

---
+++ src/redux/rocketSlice.js
@@ -0,0 +1,44 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = {
+  loading: false,
+  value: [],
+  error: '',
+};
+const ROCKETURL = 'https://api.spacexdata.com/v4/rockets';
+
+export const fetchRockets = createAsyncThunk('fetchRockets', async () => {
+  const response = await axios.get(`${ROCKETURL}`);
+  const rockets = response.data.map((data) => ({
+    id: data.id,
+    name: data.name,
+    description: data.description,
+    image: data.flickr_images[0],
+    reserved: false,
+  }));
+  console.log(rockets);
+  return rockets;
+});
+
+const rocketSlice = createSlice({
+  name: 'rockets',
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(fetchRockets.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchRockets.fulfilled, (state, action) => {
+      state.loading = false;
+      state.value = action.payload;
+      state.error = '';
+    });
+    builder.addCase(fetchRockets.rejected, (state, action) => {
+      state.loading = false;
+      state.value = [];
+      state.error = action.error.message;
+    });
+  },
+});
+
+export default rocketSlice.reducer;
