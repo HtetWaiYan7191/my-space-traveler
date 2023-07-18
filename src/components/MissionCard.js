bbc4dc998bbc9cdc078e1a44dccece3d8041e7c1
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-18 14:34:06 +0630

    Add basic design and structure for mission card

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import "../styles/MissionCard.css";
 
 const MissionCard = ({ mission }) => (
-  <div>
-    {mission.id}
-    {mission.name}
-    {mission.description}
-  </div>
+  <tr className="table-row flex items-start">
+    <th className=" w-[10%]">{mission.name}</th>
+    <th className="description-row">{mission.description}</th>
+    <th className=" w-[15%]"><span className="  bg-gray-300 rounded-md px-1">NOT A MEMBER</span></th>
+    <th className=" w-[13%]">
+        <button type="button" className=" px-1 py-1 border border-slate-700 rounded-md text-gray-500">Join Mission</button>
+    </th>
+  </tr>
 );
 
 MissionCard.propTypes = {
