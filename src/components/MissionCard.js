e950be3bcbb31d5b87d52b2f6077a5536e1430bf
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-18 15:51:06 +0630

    Implement conditional rendering

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -1,17 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "../styles/MissionCard.css";
+import { useDispatch } from "react-redux";
+import { joinMission, leaveMission } from "../redux/missionSlice";
 
-const MissionCard = ({ mission }) => (
+function MissionCard ({ mission }){
+    const dispatch = useDispatch();
+    const isJoined = mission.joined;
+
+    const handleClick = () => {
+        if(!isJoined) {
+            dispatch(joinMission(mission.id))
+        } 
+        else {
+            dispatch(leaveMission(mission.id))
+        }
+    }
+    return (
   <tr className="table-row flex items-start">
     <th className=" w-[10%]">{mission.name}</th>
     <th className="description-row">{mission.description}</th>
-    <th className=" w-[15%]"><span className="  bg-gray-300 rounded-md px-1">NOT A MEMBER</span></th>
-    <th className=" w-[13%]">
-        <button type="button" className=" px-1 py-1 border border-slate-700 rounded-md text-gray-500">Join Mission</button>
+    <th className=" w-[17%]"><span className={!isJoined ? ` bg-gray-400 rounded-md px-2 py-1` : `bg-green-500 text-white rounded-md px-1`}>{isJoined ? 'Active Member' : 'Not a Member'}</span></th>
+    <th className=" w-[15%]">
+        <button type="button" className={isJoined ? `border px-2 py-1 rounded-md border-red-500 text-red-500` : ` px-1 py-1 border border-slate-700 rounded-md text-gray-500`} onClick={handleClick}>{isJoined ? 'Leave Meeting ' : 'Join Meeting'}</button>
     </th>
   </tr>
-);
+)};
 
 MissionCard.propTypes = {
   mission: PropTypes.objectOf(PropTypes.string).isRequired,
