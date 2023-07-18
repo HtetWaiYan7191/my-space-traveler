14c26c64be4a94ec0d95a367283dd9295701477b
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-18 11:13:43 +0630

    Add Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import planet from '../assets/planet.png';
+import '../styles/Navbar.css';
 
-const Navbar = () => {
+function Navbar() {
   return (
-    <div>
-      
-    </div>
-  )
+    <nav className=" flex justify-between px-10 items-center border-b">
+      <div className="logo-container flex text-4xl font-semibold align-middle items-center">
+        <img src={planet} width="80px" height="80px" alt="logo" className="me-4" />
+        <h2>Space Travelers' Hub</h2>
+      </div>
+      <ul className="flex">
+        <li className="me-2">
+          <NavLink to="/">Rockets</NavLink>
+
+        </li>
+        <li className="me-2">
+          <NavLink to="/missions">Missions</NavLink>
+        </li>
+        <li className="me-2">
+          <NavLink to="/myprofile">My Profile</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
