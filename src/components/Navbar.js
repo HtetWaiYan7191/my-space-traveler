c26cf76e367ea18530fffeb8a9008c3e67d1d862
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-21 14:27:51 +0630

    Add vertical line in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import '../styles/Navbar.css';
 
 function Navbar() {
   return (
-    <nav className=" flex justify-between px-10 items-center border-b">
+    <nav className=" flex justify-between px-10 items-center border-b py-5">
       <div className="logo-container flex text-4xl font-semibold align-middle items-center">
         <img src={planet} width="80px" height="80px" alt="logo" className="me-4" />
         <h2>Space Travelers&apos; Hub</h2>
@@ -18,6 +18,7 @@ function Navbar() {
         <li className="me-2">
           <NavLink to="/missions">Missions</NavLink>
         </li>
+        <div className=' vertical-line me-2'></div>
         <li className="me-2">
           <NavLink to="/myprofile">My Profile</NavLink>
         </li>
